bac093a8426b8e1a18a575a8ad99c0b538200487
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-11 07:26:01 +0900

    Update soruto_ejb.js

--- bin/soruto_ejb.js
+++ bin/soruto_ejb.js
@@ -10,3 +10,9 @@ function getId(s){
 var r = document.getElementById(s);
 return r;
 }
+function sIdValue(i,s){
+	var id = document.getElementById(i).value = s;
+}
+function cl(s){
+	console.log(s);
+}
