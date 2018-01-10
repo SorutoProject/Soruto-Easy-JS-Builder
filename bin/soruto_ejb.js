b73f5b5eac5a3af0d44eeffebd841c615f567eac
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-10 21:29:27 +0900

    Main
    This Script is plain,not min.

---
+++ bin/soruto_ejb.js
@@ -0,0 +1,12 @@
+/*Soruto Easy JS Builder beta
+*caution:This script is beta version, so this script may have bugs.
+*Please understand it.
+*(C)2018 Soruto Project.
+*/
+function al(s){
+alert(s);
+}
+function getId(s){
+var r = document.getElementById(s);
+return r;
+}
