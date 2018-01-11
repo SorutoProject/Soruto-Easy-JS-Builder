50e984409c3ee07210582078e1fd9329ae8d93e2
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-11 23:54:50 +0900

    Add files via upload

--- js/soruto_ejb.js
+++ js/soruto_ejb.js
@@ -22,19 +22,20 @@ function So_cl(s){
 }
 function So_getDT(){
 var dm = new Date();
-var ye = dm.getYear();
+var ye = dm.getFullYear();
 var mo = dm.getMonth() + 1;
 var date= dm.getDate();
 var ho = dm.getHours();
 var min = dm.getMinutes();
 var sec = dm.getSeconds();
-var dat = ye + " " + mo + " " + date + " " + ho + " " + min + " " + sec;
+var day = dm.getDay();
+var dat = ye + " " + mo + " " + date + " " + ho + " " + min + " " + sec + " " + day;
 var da = dat.split(" ");
 return da;
 }
 function So_sub(n){
 	eval("document."+ n +".submit();");
 }
-function So_IdFoc(i){
+function So_Foc(i){
 	eval("document.getElementById(\""+i+"\").focus();");
 }
