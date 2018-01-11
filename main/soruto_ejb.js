38539810b93bfa6ba1669ef68d601303720eb3e8
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-11 22:38:05 +0900

    Create soruto_ejb.js

---
+++ main/soruto_ejb.js
@@ -0,0 +1,18 @@
+/*Soruto Easy JS Builder beta
+*caution:This script is beta version, so this script may have bugs.
+*Please understand it.
+*(C)2018 Soruto Project.
+*/
+function al(s){
+alert(s);
+}
+function getId(s){
+var r = document.getElementById(s);
+return r;
+}
+function sIdValue(i,s){
+	var id = document.getElementById(i).value = s;
+}
+function cl(s){
+	console.log(s);
+}
