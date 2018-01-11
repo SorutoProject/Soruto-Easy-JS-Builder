30b4da9b547a8ae33f67b861d7f4655e9f8a1e46
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-01-11 23:12:30 +0900

    Update soruto_ejb.js

--- js/soruto_ejb.js
+++ js/soruto_ejb.js
@@ -10,17 +10,17 @@ function So_gId(s){
 var r = document.getElementById(s);
 return r;
 }
-function So_IdSetValue(i,s){
+function So_SetVal(i,s){
 	document.getElementById(i).value = s;
 }
-function So_IdGetValue(i){
+function So_GetVal(i){
 	var v = document.getElementById(i).value;
 	return v;
 }
 function So_cl(s){
 	console.log(s);
 }
-function S_getDT(){
+function So_getDT(){
 var dm = new Date();
 var da;//da変数を定義
 var da[0] = dm.getYear();
@@ -29,4 +29,11 @@ var da[2] = dm.getDate();
 var da[3] = dm.getHours();
 var da[4] = dm.getMinutes();
 var da[5] = dm.getSeconds();
+return da;
+}
+function So_sub(n){
+	eval("document."+ n +".submit();");
+}
+function So_IdFoc(i){
+	eval("document.getElementById(\""+i+"\").focus();");
 }
